b16dbb0870f0e2c23e06c0019bc9c2258c2c7b13
kennedyuc <chukwukakennedyuche@gmail.com>
2024-03-08 20:42:11 +0100

    added workflow script

--- terraform-string-dispatch/variables.tf
+++ terraform-string-dispatch/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "storage_account_name" {
     description = "Azure Storage Account name"
     type        = string
-    default     = "ghatestactionstringstorage"
+    default     = "ghatestactionstrstorage"
 }
 
 variable "storage_tier" {

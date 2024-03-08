5d9e9c311d5f63f78195a68a5fc4a6d1eaa19646
kennedyuc <chukwukakennedyuche@gmail.com>
2024-03-08 18:46:09 +0100

    added workflow script

--- terraform-scripts/backend.tf
+++ terraform-scripts/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name     = "gha-action-grp"
-    storage_account_name    = "ghactionstorage"
-    container_name          = "gha-actions-storage"
+    resource_group_name     = azurerm_resource_group.rg.name
+    storage_account_name    = azurerm_storage_account.storage.name
+    container_name          = azurerm_storage_container.container.name
     key                     = "terraform.tfstate"
   }
 }

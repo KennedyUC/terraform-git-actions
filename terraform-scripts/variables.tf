8846a521d421498f37be833ea2ea2a3307b4d0f7
kennedyuc <chukwukakennedyuche@gmail.com>
2024-03-08 19:23:37 +0100

    added workflow script

--- terraform-scripts/variables.tf
+++ terraform-scripts/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "storage_account_name" {
     description = "Azure Storage Account name"
     type        = string
-    default     = "ghactionstorage"
+    default     = "ghatestactionstorage"
 }
 
 variable "storage_tier" {

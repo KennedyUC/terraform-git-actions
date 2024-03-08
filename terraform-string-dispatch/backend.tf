f3fa28607e129c4f2ed1f1d3e91bbd7569590fc4
kennedyuc <chukwukakennedyuche@gmail.com>
2024-03-08 20:40:13 +0100

    added workflow script

--- terraform-string-dispatch/backend.tf
+++ terraform-string-dispatch/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name     = "gha-test-grp"
     storage_account_name    = "ghateststoragetf"
-    container_name          = "gha-tf-state"
+    container_name          = "gha-tf-string-state"
     key                     = "terraform.tfstate"
   }
 }
--- terraform-string-dispatch/variables.tf
+++ terraform-string-dispatch/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "storage_account_name" {
     description = "Azure Storage Account name"
     type        = string
-    default     = "ghatestactionstorage"
+    default     = "ghatestactionstringstorage"
 }
 
 variable "storage_tier" {
